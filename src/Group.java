101e9bb7d031d04411aff1df626142364e040107
laroyenneg <laroyenneg@gmail.com>
2018-10-23 09:23:17 +0200

    class

---
+++ src/Group.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Group {
+
+    private List<Client> clientList;
+
+
+}
---
+++ src/Server.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Server {
+
+    private List<Client> clientList;
+
+}
